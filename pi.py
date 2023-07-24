910171fb8e21be607b3f664bd5382e6fd9e64097
Namki Kwon <75561206+namgi1234@users.noreply.github.com>
2023-07-25 00:17:23 +0900

    Update pi.py

--- pi.py
+++ pi.py
@@ -1,8 +1,5 @@
-import math
-
-number_of_side = int(input())
-
-angle = 180/number_of_side
-
-pi = math.sin(math.radians(angle))*number_of_side
-print(pi)
+n = int(input())
+s=0
+for k in range(n):
+  s = s + (1/n) * ( (1-(k/n)**2)**.5 ) 
+print(s*4)
