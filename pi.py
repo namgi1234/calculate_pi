15c5fc299876ddfc2ddbb54af4a559a96551d929
Namki Kwon <75561206+namgi1234@users.noreply.github.com>
2023-07-25 00:14:37 +0900

    Update pi.py

--- pi.py
+++ pi.py
@@ -2,8 +2,7 @@ import math
 
 number_of_side = int(input())
 
-angle = 360/number_of_side
+angle = 180/number_of_side
 
-pi = (math.sin(math.radians(angle))*number_of_side)/2
-
+pi = math.sin(math.radians(angle))*number_of_side
 print(pi)
